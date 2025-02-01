a93756b9b04d379525217a3e24375e1d76aa0e46
Shubhankan Sharma <67692943+Shubhankan-Sharma@users.noreply.github.com>
2025-02-01 13:33:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ safety_status_mapping = {
     1: "Caution",
     2: "Danger"
 }
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     input_data = data.get('input', [])
-
+    
     # Validate input data
     try:
         input_data = [float(i) for i in input_data]  # Convert to float
@@ -26,20 +27,11 @@ def predict():
 
     # Make prediction
     prediction = model.predict([input_data])
-
+    
     # Map prediction to safety status
     safety_status = safety_status_mapping.get(prediction[0], "Unknown")
-    # Assuming the prediction returns a string directly
-    safety_status = prediction[0]  # Use the prediction directly
-    # Return a detailed response
-    response = {
-        'safety_status': safety_status,
-        'input_data': input_data,
-        'message': f'The beach safety status for the provided input is: {safety_status}.'
-    }
 
     return jsonify({'safety_status': safety_status})
-    return jsonify(response)
 
 # Run the application with Gunicorn in production
 if __name__ == '__main__':
